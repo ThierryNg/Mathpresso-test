54f437020de1d067060d98c6220f8896742a8cbf
ThierryNg <ng.thierry.d@gmail.com>
2019-07-01 16:02:21 +0900

    adding project with Alamofire

---
+++ Mathpresso-test/Mathpresso-test/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Mathpresso-test
+//
+//  Created by Thierry Ng on 01/07/2019.
+//  Copyright © 2019 Thierry Ng. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
